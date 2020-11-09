f9d45c29ae7c485050d9c53ab24e3e6008e97620
AlainDevis <devisalain04@gmail.com>
2020-11-09 10:42:30 +0200

    Defining the Goal-Detail

--- QuoteApp/src/app/quote-detail/quote-detail.component.ts
+++ QuoteApp/src/app/quote-detail/quote-detail.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter} from '@angular/core';
+import {Quote} from '../quote';
 
 @Component({
   selector: 'app-quote-detail',
@@ -7,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class QuoteDetailComponent implements OnInit {
 
+  @Input() quote:Quote;
   constructor() { }
 
   ngOnInit(): void {
