8e1bb3b0cfb4dac0901f35600b560581471d0198
AlainDevis <devisalain04@gmail.com>
2020-11-09 11:55:34 +0200

    Creating the Downvotes Function

--- QuoteApp/src/app/app.module.ts
+++ QuoteApp/src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { FormsModule } from '@angular/forms';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { QuoteComponent } from './quote/quote.component';
@@ -16,7 +18,8 @@ import { QuoteDetailComponent } from './quote-detail/quote-detail.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
